edd6349433085a93db9810f807125f50b978893e
ludmilact <ludmilatimoteo@Outlook.com>
2022-08-01 02:32:17 -0300

    Classe Principal desenvolvida por Emerson, Luiz Fernando e Ludmila.

---
+++ src/entities/Principal.java
@@ -0,0 +1,10 @@
+/**
+ Classe desenvolvida por Emerson, Luiz Fernando e Ludmila.
+ */
+package entities;
+
+public class Principal {
+    public static void main(String[] args) {
+        Menu.menuInicial();
+    }
+}
